b9e64ad538c179b7abe359ded497cdbe23d038bf
Leon Polak <lp0808@gmail.com>
2024-04-05 10:32:22 +0300

    Vite config for https

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import fs from 'fs';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    https: {
+      key: fs.readFileSync('./.cert/key.pem'),
+      cert: fs.readFileSync('./.cert/cert.pem'),
+    }
+  },
 })
